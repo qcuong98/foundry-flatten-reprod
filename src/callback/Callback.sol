cfd2b2018beb9d0eeabb160159e984c87bead275
Cuong Tran <tranquoccuong@outlook.com.vn>
2025-05-08 14:13:04 +0700

    fix: change callback exposure

--- src/callback/Callback.sol
+++ src/callback/Callback.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.28;
 import {ICallback} from "../interface/ICallback.sol";
 
 contract Callback is ICallback {
-    function swapCallback(int256 amountToPay, int256 amountOut, bytes calldata) external view {
+    function swapCallback(int256 amountToPay, int256 amountOut, bytes calldata) external pure {
         require(amountToPay != 0 && amountOut != 0, "In or Out Amount is Zero");
     }
 }
